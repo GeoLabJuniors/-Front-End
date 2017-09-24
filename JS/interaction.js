65019d2fa3bf0a24f497b1837e378e0ba1d87763
Guram Sanikidze <sanikidze.guram@gmail.com>
2017-09-24 12:12:07 +0400

    Almost whole Homepage
    Updated document structure. Addet Bootstrap, jquery and some js files for printing html. Addet interaction and includes responsive.

---
+++ JS/interaction.js
@@ -0,0 +1,5 @@
+/*--- candidat hover ---*/
+
+$(".candidat").hover(function () {
+    $(this).find('.img-candidat').toggleClass("hvr-yellow-back");
+});
---
+++ JS/printHeader.js
@@ -0,0 +1,2 @@
+let header = '<div class="main-container"><div id="logo"></div><nav id="header-nav"><button class="hvr-back-pulse btn-font">ავტორიზაცია</button><button class="btn-font button-active hvr-back-pulse">რეგისტრაცია</button><img src="img/burger.svg" alt="navigation" id="burger-icon"></nav></div>';
+document.querySelector('header').innerHTML = header;
