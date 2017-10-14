153271b336a9989adcaab1b195a327f597b3cb41
Guram Sanikidze <sanikidze.guram@gmail.com>
2017-10-14 21:19:51 +0400

    Added Candidats Page
    needed filters only

--- JS/interaction.js
+++ JS/interaction.js
@@ -3,3 +3,14 @@
 $(".candidat").hover(function () {
     $(this).find('.img-candidat').toggleClass("hvr-yellow-back");
 });
+
+/*--- filter ---*/
+if (!isTouchDevice()) {
+    $('[data-toggle*="tooltip"]').tooltip();
+  }
+  
+  // utility
+  
+  function isTouchDevice() {
+      return !!('ontouchstart' in window || navigator.msMaxTouchPoints);
+  }
