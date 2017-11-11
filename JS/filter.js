748ed7eccbe107cd6687333d3eb14fd0b5f5357b
Guram Sanikidze <sanikidze.guram@gmail.com>
2017-11-11 19:53:25 +0400

    added show/hide filter function

--- JS/filter.js
+++ JS/filter.js
@@ -1,3 +1,16 @@
-function showFilter(){
-    alert('clicked');
+let filterName = document.querySelectorAll('.filter-name');
+for (const i of filterName) {
+    i.addEventListener('click', (e) => {
+        hideFilter(e);
+    });
+}
+
+function hideFilter(e){
+    if(e.srcElement.className === 'possibilities-headline'){  
+        $('.possibilities').toggleClass('hide');
+        $($('.possibilities-section').find('img')[0]).toggleClass('arrow-up')
+    } else if(e.srcElement.className === 'rating-headline'){
+        $('.rating-from-to').toggleClass('hide');
+        $($('.rating-section').find('img')[0]).toggleClass('arrow-up')
+    }
 }
