ea9f59ca3125a665ef89169079ab18198ec5c3d9
Anano Aspanidze <anano.aspanidze@gmail.com>
2017-11-22 19:05:42 +0400

    same

--- JS/printCandidats.js
+++ JS/printCandidats.js
@@ -1,8 +1,60 @@
 /*--- add candidats ---*/
 var allCandidats = document.querySelectorAll('.candidat');
 
+
+var names = ["ანა","ცოტნე","ნიკა","გიორგი","ავთანდილ", "გიგა", "ზვიად", "ცოტნე"];
+
+var surnames = ["კეკუა","გვაჯაია","ახალაია","ქოჩიაშვილი","გაჩეჩილაძე", "ლაფანაშვილი", "ჭანტურია", "გუჩუა"];
+
+var photos = ["ana-kekua.jpg","tsotne-gvajaia.jpg","nika-akhalaia.png","giorgi-qochiashvili.jpg","avtandil-gachechiladze.jpg", "giga-lafanashvili.jpg", "zviad-tchanturia.jpg", "tsotne-guchua.jpg"];
+
+var positions = ["Front End დეველოპერი","Front End დეველოპერი","Front End დეველოპერი","Front End დეველოპერი","Back End დეველოპერი", "Back End დეველოპერი", "Back End დეველოპერი", "Back End დეველოპერი"];
+
+var bios = ["<p><span>მე ვარ ანა კეკუა, 17 წლის. ვცხოვრობ ქალაქ ზუგდიდში. ვსწავლობ აფხაზეთის N1 საჯარო სკოლაში.</span></p><p><span>2014 წელს გავიარე &bdquo;სამშვიდობო განათლების&ldquo; 6 თვიანი ტრენინგ-კურსი და მონაწილეობა მივიღე პროექტში &bdquo;მოსწავლე ახალგაზრდობის სამშვიდობო განათლება ნდობის აღდგენისთვის&ldquo;.</span></p><p><span>2016 წელს გავიარე &bdquo;English Access Microscolarship Program&ldquo;, ინგლისურის ენის შესწავლის სასტიპენდიო პროგრამა.</span></p><p><span>2016 წელს მონაწილეობა მივიღე პროექტ &bdquo;ეტალონში&ldquo;.</span></p><p><span>2016 წელს გავიმარჯვე &bdquo;ჩემი სამყაროს&ldquo; ვიქტორინაში.</span></p><p><span>2017 წელს ვმონაწილეობდი კონკურსში &bdquo;ჩაერთე შენი ქალაქის კულტურული მემკვიდრეობის &nbsp;კვლევაში&ldquo;, სადაც ავიღე პირველი ადგილი.</span></p><p><span>მიმდინარე წელს ჩართული ვარ ჯეოლაბის პროექტში &bdquo;ვებპროგრამირების კურსი რეგიონებისათვის&ldquo;.</span></p>",
+"<p>ბიოგრაფია - დაბადების თარიღი, რიცხვი, თვე, წელი: 10/07/1990<br />ამჟამინდელი საცხოვრებელი ადგილი: ზუგდიდი<br />ტელ.: 568912248 ელ.ფოსტა:tsotne.gvajaia@gmail.com<br />IT სპეციალისტი</p><p><br />განათლება:</p><p>სასწავლებლის დასახელება: საქართველოს ტექნიკური უნივერსიტეტი&nbsp;<br />სწავლების წლები:2008-2012<br />ფაკულტეტი:ინფორმატიკისა და მართვის ავტომატიზებული სისტემები<br />ხარისხი:ბაკალავრი</p><p>&nbsp;</p><p><br />სამუშაო გამოცდილება:</p><p>1.ორგანიზაციის დასახელება: Service +<br />&nbsp; თანამდებობა: IT - სპეციალისტი<br />&nbsp; ფუნქცია-მოვალობები:ორგანიზაციებში ქსელისა და კომპიუტერების გამართვა როგორც პროგრამულად ასევე ტექნიკურადაც ..<br />&nbsp; მუშაობის პერიოდი:2012-2014<br />2.ამჟამად ვარ თვითდასაქმებული IT სფეროში (კომპიუტერებისა და სმარტფონების &nbsp;&nbsp;&nbsp;&nbsp;პროგრამული გამართვა) აგრეთვე კლინიკაში IT სპეციალისტად<br /><br /><br /><br />კვალიფიკაციის ასამაღლებელი კურსები(ტრეინინგები):<br /><br />1.ტრეინინგის დასახელება:კომპიუტერების პროგრამული და აპარატურული გამართვა<br />&nbsp; ტრეინინგის ჩატარების პერიოდი:2012-2012<br />&nbsp; ორგანიზაციის დასახელება:კომპანია SERVICE +<br />&nbsp; ტრეინინგის ჩატარების ადგილი:თბილისი<br />2. ტრეინინგის დასახელება: HTML/CSS/JAVACSRIPT/ შემსწავლელი კურსები<br />&nbsp; &nbsp;ტრეინინგის ჩატარების პერიოდი:2017 დან დღემდე<br />&nbsp; &nbsp;ორგანიზაციის დასახელება:GEOLAB<br />&nbsp; &nbsp;ტრეინინგის ჩატარების ადგილი:Tbilisi</p>",
+"<p>ასაკი : 15<br />საცხოვრებელი ადგილი: ზუგდიდი, სოფ. დარჩელი<br />სკოლა : სოფ.დარჩელის №1 საჯარო<br />ტრენინგები: ზუგდიდში, ტექნოპარკში, Front-end 1 თვიანი კურსი.(ამ ეტაპისთვის მხოლოდ ესაა ჯეოლაბის გარდა)<br />სამომავლო გეგმები: ამ ეტაპისთვის ვაპირებ back end ის სწავლას, კერძოდ php. შემდგომში დიდი ალბათობით ვაპირებ ამ განხრით ჩაბარებას უმაღლეს სასწავლებელში.<br />&nbsp;</p>",
+"<p>ვარ 15 წლის. დავიბადე ხარაგაულის რაიონის სოფელ წყალაფორეთში. ვსწავლობ ილმის საჯარო სკოლაში.</p><p>მონაწილეობა მაქვს მიღებული &ldquo;ახალგაზრდები ინოვაციური განვითარებისთვის&rdquo;. გავიმარჯვე კრეათონში ხარაგაულის ინოვაციების ცენტრში.</p><p>ამჟამად &nbsp;ვიღებ მონაწილეობას &ldquo;ვებ პროგრამირების კურსი რეგიონებისთვის&rdquo;. ასევე, ვსწავლობ პროგრამირების ენა php-ის ჯეოლაბში და გავდივარ სტაჟირებას ჯეოლაბში.</p><p>დაინტერესეული ვარ ლაშქრობებით, ნადირობით და წიგნების კითხვით.</p><p>მომავალში ვაპირებ, რომ უფრო გავიღრმაო ჩემი ცოდნა პროგრამირების მხრივ და რაღაც სერიოზული შევქმნა, რაც ხალხს დააინტერესებს.</p>",
+"<p>ავთანდილ გაჩეჩილაძე (themarshall)<br />17 წლის<br />საცხოვრებელი ადგილი: &nbsp;ბაღდათი<br />როხის საჯარო სკოლის მოსწავლე..(2006-...)<br />ონლაინ კურსები ქოდაქადემიზე Front End-ის განხრით..</p><p>ჯეოლაბის Back End-ის პროგრამირების კურსი..</p><p>არასამთავრობო ორგანიზაცია youth2georgia-სთან ერთად გავლილი მაქვს ფოტო და ვიდეო გადაღების, ადვოკატირებისა და ლიდერობის ტრენინგები..<br />არდუინოს პროგრამირების კურსსაც გავდივარ და გეგმაში გვაქვს რობოტის შექმნა ბაღდათის ინოვაციების ცენტრში..<br />მომავალში ვაპირებ სწავლის გაგრძელებას პროგრამირების განხრით, როგორც უნივერსიტეტში, ისე მის გარეთ..<br />&nbsp;</p>", 
+"<p>ვარ 17 წლის, &nbsp;ვცხოვრობ დუშეთის რაიონის სოფელ ჭოპორტში.</p><p>ამჟამად ვსწავლობ ჭოპორტის საჯარო სკოლის მე-12 კლასში და გავდივარ ჯეოლაბის ვებპროგრამირების კურსს. ასევე, გავლილი მაქვს გაცნობითი ტრენინგი კომპიუტრულ პროგრამირებაში, გრაფიკულ დიზაინსა და სოციალურ მედიასთან დაკავშირებით.</p><p>ვმონაწილეობდი მათემატიკისა და სამოქალაქო განათლების ოლიმპიადებში, მაქვს სიგელები, ვიყავი &bdquo;ინტელექტ- ჩემპიონატ ეტალონის&ldquo; რეგიონალური ფინალური ტურების მონაწილე და პრიზიორი, აგრეთვე ავიღე პირველი ადგილი საქართველოს პარლამენტის ეროვნული ბიბლიოთეკის მიერ ჩატარებულ ინტელექტუალურ თამაშში &bdquo;ჩვენი მკითხველი&ldquo;.</p><p>დაინტერესებული ვარ მიკროელექტროობითა და კომპიუტერული მეცნიერებებით, ვაპირებ ამავე მიმართულებით გავაგრძელო სწავლა უნივერსიტეტში.<br />&nbsp;</p>", 
+"<p>ვცხოვრობ ქალაქ თბილისში. ვარ 24 წლის.</p><p>ვსწავლობდი &nbsp;შოთა რუსთაველის სახელმწიფო უნივერსიტეტში ფიზიკა-მათემატიკის და კომპიუტერული მეცნიერებების ფაკულტეტზე 2012-17 წლებში.</p><p>გავლილი მაქვს IT პროექტების მენეჯერის ტრენინგი.</p><p>ამჟამად ვსწავლობ ჯეოლაბში ვებპროგრამირებას და ვმუშაობ ლივინგსტონში ვებდეველოპერად.</p>", 
+"<p>ვარ 18 წლის, ვცხოვრობ ქალაქ ხობში, ამჟამად კი თბილისში.</p><p>დავამთავრე სსიპ ქ. ხობის #2 საჯარო სკოლა (2005-2017).</p><p>ჩავაბარე - &nbsp;თბილისის სახელმწიფო სამედიცინო უნივერსიტეტში, მედიცინის ფაკულტეტზე (2017).</p><p>ვიღებდი მონაწილეობას სახელმწიფო ოლიმპიადებში და პროექტებში: &ldquo;ალიანტე&ldquo;, &nbsp;&bdquo;წიგნების თარო&ldquo;(სატელევიზიო შოუ).</p><p>მაინტერესებს კომპიუტერული მეცნიერებები და მედიცინა.</p><p>მსურს, საფუძვლიანად შევისწავლო კომპ. მეცნიერერები და შევძლო ამ სფეროს განვითარებაში წვლილის შეტანა.</p>"];
+
+var skills = ["HTML,CSS,JavaScript,jQuery,Bootstrap",
+"HTML,CSS,JavaScript,jQuery,Bootstrap",
+"HTML,CSS,JavaScript,jQuery,Bootstrap,WordPress",
+"HTML,CSS,JavaScript,jQuery,Bootstrap,WordPress",
+"HTML,CSS,C<span style='font-family:Roboto'>#</span>,ASP .NET MVC,MS SQL",
+"C<span style='font-family:Roboto'>#</span>,ASP .NET MVC,MS SQL",
+"C<span style='font-family:Roboto'>#</span>,ASP .NET MVC,MS SQL,Unity",
+"C<span style='font-family:Roboto'>#</span>,ASP .NET MVC,MS SQL"];
+
+var projectQuantities = [3,2,1,4,0,0,0,0];
+
+var projectInProgressQuantities = [1,1,1,3,0,0,0,0];
+
+var ratings = [10,10,10,10,10,10,10,10];
+
+
+var i = 0;
 allCandidats.forEach(function(e){
-    let r = Math.floor(Math.random() * (4 - 1) + 1);
-    let candidatHTML = `<div><a href="profile.html"><img class="img-thumbnail img-candidat" src="img/pp/c${r}.jpg" alt="profile picture"></a><div class="candidat-text-content"><a href="profile.html"><h3 class="candidat-name-fn">სახელი გვარი</h3></a><ul class="candidat-skils-fn"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Javascript</a></li></ul><ul class="candidat-statistics-fn candidat-statistics"><li>განხორციელებული პროექტები: <a href="#">480</a></li><li>რეიტინგი: 89 / 100</li></ul></div></div>`;    
-    e.innerHTML = candidatHTML;
+    let candidateSkills = skills[i].split(",");
+    let candidateSkillsHtml = `<ul class="candidat-skils-fn">`;
+    
+candidateSkills.forEach(function(skill){
+	candidateSkillsHtml+=`<li><a href="#">` + skill +`</a></li>`;
 });
+    candidateSkillsHtml+=`</ul>`;
+    let candidatHTML = `<div><a href="profile-`+ names[i]+`-`+ surnames[i]+ `.html" class="image-a" style="background-image: url(./img/pp/`+ photos[i] + `);"><img class="img-thumbnail img-candidat" src="img/pp/` + photos[i] +`" alt="profile picture"></a><div class="candidat-text-content"><a href="profile-`+ names[i]+`-`+ surnames[i]+ `.html"><h3 class="candidat-name-fn">` + names[i] + ` ` + surnames[i] + `</h3></a>` +candidateSkillsHtml+ `<ul class="candidat-statistics-fn candidat-statistics"><li>განხორციელებული პროექტები: <a href="#">` + projectQuantities[i] +`</a></li><li>რეიტინგი: `+ratings[i]+` / 10</li></ul></div></div>`;    
+    e.innerHTML = candidatHTML;
+    i++;
+});
+
+
+
+// გურამის კოდი
+
+// allCandidats.forEach(function(e){
+//     let r = Math.floor(Math.random() * (4 - 1) + 1);
+//     let candidatHTML = `<div><a href="profile.html"><img class="img-thumbnail img-candidat" src="img/pp/c${r}.jpg" alt="profile picture"></a><div class="candidat-text-content"><a href="profile.html"><h3 class="candidat-name-fn">სახელი გვარი</h3></a><ul class="candidat-skils-fn"><li><a href="#">HTML</a></li><li><a href="#">CSS</a></li><li><a href="#">Javascript</a></li></ul><ul class="candidat-statistics-fn candidat-statistics"><li>განხორციელებული პროექტები: <a href="#">480</a></li><li>რეიტინგი: 89 / 100</li></ul></div></div>`;    
+//     e.innerHTML = candidatHTML;
+// });
