adc216ac3045bd9458bf192febed7f3122f3288f
beqab <beka.badagadze@geolab.edu.ge>
2018-01-20 16:28:09 +0400

    დავამატე rating range

--- JS/filter.js
+++ JS/filter.js
@@ -64,6 +64,48 @@ for (const i of checkbox) {
 // }
 
 
+// rating range=========================
+     var test=0
+     var test2=200
+     console.log($('#line').offset().left)
+     var b = function (e){
+         var x=Math.round(e.clientX-$('#line').offset().left);
+         var y=Math.round(e.clientX-$('#line').offset().left);
+          var koepicienti=0.5;
+          var nashti=100-koepicienti*x;
+            if(x-test > test2-x){
+          if (x*1>=0 && x<=200  ) {
+            test2=x 
+              document.getElementById('button2').style.right=-200-(x-15)+"px"
+                document.getElementById('rating_hight').value=Math.round(100-nashti)
+                document.getElementById('buutonaut2').style.width=200+$('#line').offset().left-e.clientX+"px"
+              }
+
+            }
+            else {
+         if (x*1>=0 && x<=200  ) {
+            test=x
+              document.getElementById('button1').style.left=(x-5)+"px"
+              document.getElementById('rating_low').value=Math.round(100-nashti)
+              document.getElementById('buutonaut').style.width=x+"px"
+              };
+            }
+    }  
+
+    document.getElementById('line').onmousedown=function(e){
+                b(e);
+                document.getElementById('line').onmousemove=b;
+                document.getElementById('line').onmouseup=function(e){
+                    document.getElementById('line').onmousemove = null;
+                }
+    }
+    $('.clear-search').click(function(){
+     
+     console.log('test')
+
+    })
+
+
 // clear filter
 let clearButton = document.getElementsByClassName('clear-search')[0];
 let checkboxLength = $(document.getElementsByClassName('possibilities')[0]).find('img').length;
@@ -73,6 +115,9 @@ clearButton.addEventListener('click', (e) => {
     for(i = 0; i < checkboxLength; i++){
         $(document.getElementsByClassName('possibilities')[0]).find('img')[i].className = 'checkbox';
     }
+    document.getElementById('buutonaut2').style.width=0+"px";
+    document.getElementById('buutonaut').style.width=0+"px";
+    document.getElementById('button1').style.left=-3+"px";
     
     document.getElementsByClassName('rating-from-to')[0][0].value = '0';
     document.getElementsByClassName('rating-from-to')[0][1].value = '100';
